3ccea91f6d3b29027502da01950db4a161e4c152
Nicaise Kassi <ettekassi@gmail.com>
2025-10-08 03:40:34 +0000

    🗑️ Suppression temporaire TP/ et MkDocs/ (résolution cadenas GitHub)

--- MkDocs/manuel-utilisateur/start_mkdocs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/kassi/Documents/EXPERTISE/FORMATION_DGE_CLIMADA/Formation_CLIMADA/MkDocs/manuel-utilisateur
-/home/kassi/anaconda3/envs/climada_formation/bin/mkdocs serve --dev-addr 127.0.0.1:8000
